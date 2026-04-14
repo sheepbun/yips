7f4c35b5f6701cc7373000775f279aa768308631
sheepbun <katherinemichellenorth@gmail.com>
2026-04-14 17:30:54 -0600

    v0.1.47: Fix binary download for Linux

--- postinstall.js
+++ postinstall.js
@@ -5,8 +5,8 @@ const os = require('os');
 
 const isWin = os.platform() === 'win32';
 const ext = isWin ? '.exe' : '';
-const binaryName = isWin ? 'yips-windows.exe' : 'yips-linux';
-const url = `https://github.com/sheepbun/yips/releases/latest/download/${binaryName}`;
+const binaryName = isWin ? 'yips-core-windows.exe' : 'yips-core-linux';
+const initialUrl = `https://github.com/sheepbun/yips/releases/latest/download/${binaryName}`;
 const binDir = path.join(__dirname, 'bin');
 
 if (!fs.existsSync(binDir)) {
@@ -15,34 +15,36 @@ if (!fs.existsSync(binDir)) {
 
 const dest = path.join(binDir, `yips${ext}`);
 
-console.log(`Downloading Yips binary from ${url}...`);
+console.log(`Downloading Yips binary from ${initialUrl}...`);
 
-https.get(url, (res) => {
-    if (res.statusCode === 301 || res.statusCode === 302) {
-        // Follow redirect
-        https.get(res.headers.location, (redirectRes) => {
-            downloadStream(redirectRes);
-        }).on('error', handleError);
-    } else {
-        downloadStream(res);
-    }
-}).on('error', handleError);
-
-function downloadStream(res) {
-    if (res.statusCode !== 200) {
-        console.error(`Failed to download: HTTP ${res.statusCode}`);
-        process.exit(1);
-    }
-    const file = fs.createWriteStream(dest);
-    res.pipe(file);
-    file.on('finish', () => {
-        file.close();
-        if (!isWin) {
-            fs.chmodSync(dest, 0o755);
+function download(url) {
+    https.get(url, (res) => {
+        if (res.statusCode === 301 || res.statusCode === 302 || res.statusCode === 307 || res.statusCode === 308) {
+            // Follow redirect
+            if (res.headers.location) {
+                download(res.headers.location);
+            } else {
+                console.error(`Redirect without location header (status ${res.statusCode})`);
+                process.exit(1);
+            }
+        } else if (res.statusCode === 200) {
+            const file = fs.createWriteStream(dest);
+            res.pipe(file);
+            file.on('finish', () => {
+                file.close();
+                if (!isWin) {
+                    fs.chmodSync(dest, 0o755);
+                }
+                console.log('Downloaded Yips binary successfully.');
+            });
+        } else {
+            console.error(`Failed to download: HTTP ${res.statusCode}`);
+            process.exit(1);
         }
-        console.log('Downloaded Yips binary successfully.');
-    });
+    }).on('error', handleError);
 }
+
+download(initialUrl);
 
 function handleError(err) {
     console.error('Error downloading Yips:', err.message);
